ed3e80fddd62cbb942273e02b4c79c40208e82df
Jun Furuya <jfuruya.p4@gmail.com>
2018-06-20 18:22:00 +0900

    リンクマスタ、仕訳一覧用のSQL追加

--- sql/changes.sql
+++ sql/changes.sql
@@ -1,33 +1,8 @@
-CREATE DATABASE hideout;
-
-CREATE TABLE IF NOT EXISTS hideout.m_users (
-  m_user_id INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ユーザID',
-  m_user_username VARCHAR(100) NOT NULL COMMENT 'ユーザ名称',
-  m_user_hashed_password VARCHAR(256) NOT NULL COMMENT 'ハッシュ化済みパスワード'
-) engine=InnoDB COMMENT='ユーザマスタ';
-
-INSERT INTO hideout.m_users(m_user_id, m_user_username, m_user_hashed_password) VALUES ('1', 'admin', SHA2('admin', 256));
-
-CREATE TABLE IF NOT EXISTS hideout.m_link_categories (
-  m_link_category_id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'リンクカテゴリID',
-  m_user_id INT(3) NOT NULL COMMENT 'ユーザID',
-  m_link_category_name VARCHAR(100) NOT NULL COMMENT 'リンクカテゴリ名称',
-  m_link_category_display_order INT(3) NOT NULL COMMENT 'リンクカテゴリ表示順序'
-) engine=InnoDB COMMENT='リンクカテゴリマスタ';
-
+ALTER TABLE hideout.m_link_categories ADD COULMN m_user_id INT(3) NOT NULL COMMENT 'ユーザID';
 ALTER TABLE hideout.m_link_categories ADD CONSTRAINT m_user_id FOREIGN KEY (m_user_id) REFERENCES m_users(m_user_id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-CREATE TABLE IF NOT EXISTS hideout.m_links (
-  m_link_id INT(4) NOT NULL COMMENT 'リンクID',
-  m_user_id INT(3) NOT NULL COMMENT 'ユーザID',
-  m_link_category_id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'リンクカテゴリID',
-  m_link_site_name VARCHAR(100) NOT NULL COMMENT 'リンク名称',
-  m_link_url VARCHAR(200) NOT NULL COMMENT 'リンク名URL',
-  m_link_display_order INT(3) NOT NULL COMMENT 'リンク表示順序'
-) engine=InnoDB COMMENT='リンクマスタ';
-
-#ALTER TABLE hideout.m_links ADD CONSTRAINT m_user_id FOREIGN KEY (m_user_id) REFERENCES m_users(m_user_id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE hideout.m_links ADD CONSTRAINT m_link_category_id FOREIGN KEY (m_link_category_id) REFERENCES m_link_categories(m_link_category_id) ON DELETE CASCADE ON UPDATE CASCADE;
+#ALTER TABLE hideout.m_links ADD COULMN m_user_id INT(3) NOT NULL COMMENT 'ユーザID';
+ALTER TABLE hideout.m_links ADD CONSTRAINT m_user_id FOREIGN KEY (m_user_id) REFERENCES m_users(m_user_id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 CREATE TABLE IF NOT EXISTS hideout.m_languages (
   m_language_id TINYINT(2) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '言語ID',
@@ -62,3 +37,4 @@ CREATE TABLE IF NOT EXISTS hideout.t_journal_entries (
   m_journal_entry_note VARCHAR(1000) NOT NULL DEFAULT '' COMMENT '備考',
   m_journal_entry_created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '登録日'
 ) engine=InnoDB COMMENT='仕訳';
+
