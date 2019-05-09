8db49b8fcfda4a6325cde96628baf7b0485c2a38
Jun Furuya <jfuruya.p4@gmail.com>
2019-05-09 15:13:11 +0900

    slackbot がサーバで動くようにする

--- slackbot_main.py
+++ slackbot_main.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from app.service.slack_service import SlackService
+ 
+import sys, os, bottle
+
+dirpath = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(dirpath)
+os.chdir(dirpath)
+
+args = sys.argv
+os.environ['APP_ENV'] = args[1]
 
 if __name__ == "__main__":
     slack_service = SlackService()
