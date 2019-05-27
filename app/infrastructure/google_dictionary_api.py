433c60dd99c851d728bf63bdb399f5febc5260f6
Jun Furuya <jfuruya.p4@gmail.com>
2019-05-27 17:31:24 +0900

    ファイル新規作成

---
+++ app/infrastructure/google_dictionary_api.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+
+
+from app.infrastructure.base_api import BaseApi
+
+'''
+Google Dictionary Api module
+'''
+class GoogleDictionaryApi(BaseApi):
+    
+    def __init__(self, idcf_config):
+        super().__init__(idcf_config)
+        
+        pass
+
